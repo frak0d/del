d87d1758c2834ff3731043b78845b6b18fa99317
frakod <frakod@pm.me>
2022-10-25 16:36:17 +0000

    v1.0 - initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# del
+Recursively Shreds Files and Directories.
+
+### Usage :-
+```
+del [path] <passes(optional)>
+```
+
+### Installation :-
+1. `git clone https://github.com/0xB00B/del`
+2. `$CXX -std=c++20 -O2 -s -static -flto del/del.cpp -o /usr/local/bin/del && rm -r del`
+
+### Uninstallation :-
+1. `rm /usr/local/bin/del`
---
+++ del.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+using namespace std::literals;
+
+int main(const int argc, const char* argv[])
+{
+    if (argc < 2)
+    {
+        std::puts("Usage: del [path] <passes(optional)>");
+        return -1;
+    }
+    if (!fs::exists(argv[1]))
+    {
+        std::puts("\x1b[1;31mError:\x1b[0;31m Invalid Path!\x1b[0m");
+        return -2;
+    }
+    if (argc > 3)
+    {
+        std::puts("\x1b[1;33mWarning:\x1b[0;33m Ignoring Extra Arguments.\x1b[0m");
+    }
+
+    auto path = argv[1];
+    auto passes = argc>2 ? argv[2] : "1";
+    bool verbose = true;
+    
+    if (fs::is_directory(path))
+    {
+        for (auto dir_entry : fs::recursive_directory_iterator(path))
+        {
+            if (!dir_entry.is_directory())
+            {
+                auto fpath = dir_entry.path().string();
+                if (verbose) std::printf("%s\n", fpath.c_str());
+                std::system(("shred -uxzfn "s+passes+' '+fpath).c_str());
+            }
+        }
+    }
+    
+    std::system(("rm -rf "s+path).c_str());
+}
